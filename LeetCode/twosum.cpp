eedb40b93143307dff883d8a3d689154a23e5a89
minxin wang <359676107@qq.com>
2019-03-31 12:15:23 +0800

    对象和索引

--- LeetCode/twosum.cpp
+++ LeetCode/twosum.cpp
@@ -1,33 +1,39 @@
+/***********************************************************************
+*author:min and kai
+*function:找到两个加数的索引
+*time:2019-3-31
+*version:ver 1.0
+************************************************************************/
 #include<bits/stdc++.h>
 typedef long long ll;
 using namespace std;
 const int N = 0;
-class Solution 
+class Solution
 {
 public:
-	vector<int> twoSum(vector<int>&numbers, int target) 
+	vector<int> twoSum(vector<int>&numbers, int target)
 	{
 		int sz = numbers.size();     //sz为numbers这个向量中元素得个数
 		vector<int> index; //向量的新的对象为Index
 		for (int i = 0; i < sz; i++)
 			for (int j = i + 1; j < sz; j++)
 				if (numbers[i] + numbers[j] == target) //两个数得和等于目标值
-				{                                         
+				{
 					index.push_back(i + 1);  //返回i从1开始数的索引，存入index这个新的对象
 					index.push_back(j + 1);  //返回j从1开始数的索引，存入index这个新的对象
 					return index;//返回这个向量对象
 				}
-	}         
+	}
 };
 int main()
 {
 	Solution s;   //类的对象
 	vector<int> in;  //应该是这个向量的对象为in
 	int n, t;
-	cout << "输入n：" ;
+	cout << "输入n：";
 	cin >> n;
-	cout << "输入t:" ;
- 	while (n--) 
+	cout << "输入t:";
+	while (n--)
 	{
 		cin >> t;
 		in.push_back(t);   //依次将元素添加到向量中
